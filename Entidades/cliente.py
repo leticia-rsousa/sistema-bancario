73bb7fdb20b8ba80158ac12f9d2c584057210fba
Letícia Rodrigues de Sousa <007rsleticia@gmail.com>
2026-02-20 16:39:39 -0300

    "Adiciona classe Cliente com atributos, método adicionar_conta e __str__"

--- Entidades/cliente.py
+++ Entidades/cliente.py
@@ -1,19 +1,11 @@
-# Aplicação Full-Stack de Sistema Bancário em Python com POO
-# Módulo da Entidade Cliente
-
-# Define a classe Cliete
 class Cliente:
     def __init__(self, nome: str, cpf: str):
         self.nome = nome
         self.cpf = cpf
+        self.contas = []  # Lista de contas associadas ao cliente
 
-        # Lista vazia para armazenar as contas associadas ao cliente
-        self.contas = []
-
-    # Método para adicionar uma conta à lista de contas do cliente
     def adicionar_conta(self, conta):
         self.contas.append(conta)
 
-    # Método que define a representação em string do objeto
     def __str__(self):
         return f"Cliente: {self.nome} | CPF: {self.cpf}"
